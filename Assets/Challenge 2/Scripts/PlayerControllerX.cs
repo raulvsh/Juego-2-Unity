469235cb307fcce9d5229ac690086f67855d9f26
raulvsh <raulvsh@gmail.com>
2021-01-26 23:42:06 +0100

    Solucionado reto 1 "Atrapa la bola"

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,16 +5,13 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
-	public float fireRate = 0.5f;
-	public float nextFire = 0.0f;
 
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-			nextFire = Time.time + fireRate;
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
         }
     }
