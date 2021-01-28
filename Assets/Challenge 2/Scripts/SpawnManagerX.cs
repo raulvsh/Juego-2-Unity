6dc802cae80fa33288b2659983be94a17d9cdc6d
raulvsh <raulvsh@gmail.com>
2021-01-28 12:01:07 +0100

    Reto 2 y 3 juego del perro. Tiempo aleatorio de lanzamiento de pelotas. Espera de 1s para lanzar perro.

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -11,12 +11,12 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnPosY = 30;
 
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
 
     // Start is called before the first frame update
     void Start()
-    {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+    { 
+        //float spawnInterval = Random.Range(0, 5);
+        Invoke("SpawnRandomBall", startDelay);
     }
 
     // Spawn random ball at random x position at top of play area
@@ -28,6 +28,11 @@ public class SpawnManagerX : MonoBehaviour
 
         // instantiate ball at random spawn location
         Instantiate(ballPrefabs[spawnIndex], spawnPos, ballPrefabs[spawnIndex].transform.rotation);
+
+        float startDelay = Random.Range(1, 5);
+
+        Invoke("SpawnRandomBall", startDelay);
+
     }
 
 }
